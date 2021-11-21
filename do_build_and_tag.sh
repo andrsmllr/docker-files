d7288757a6c5fd04571c2e5b03d248a6848c5d7d
andrsmllr <andrsmllr@bananatronics.org>
2021-11-21 16:53:48 +0100

    Add iverilog to build and tag list.

--- do_build_and_tag.sh
+++ do_build_and_tag.sh
@@ -13,6 +13,7 @@ source ./dockerutils.sh
 cd ${root_dir}/andrsmllr-base && docker_build andrsmllr-base andrsmllr/base
 cd ${root_dir}/ghdl && docker_build ghdl-app andrsmllr/ghdl
 cd ${root_dir}/gtkwave && docker_build gtkwave-app andrsmllr/gtkwave
+cd ${root_dir}/iverilog && docker_build iverilog-app andrsmllr/iverilog
 cd ${root_dir}/klayout && docker_build klayout-app andrsmllr/klayout
 cd ${root_dir}/magic && docker_build magic-app andrsmllr/magic
 cd ${root_dir}/symbiyosys && docker_build symbiyosys-app andrsmllr/symbiyosys
@@ -24,6 +25,7 @@ cd ${root_dir}/yosys && docker_build yosys-app andrsmllr/yosys
 docker_push_tag andrsmllr/base latest
 docker_push_tag andrsmllr/ghdl latest
 docker_push_tag andrsmllr/gtkwave latest
+docker_push_tag andrsmllr/iverilog latest
 docker_push_tag andrsmllr/klayout latest
 docker_push_tag andrsmllr/symbiyosys latest
 docker_push_tag andrsmllr/verilator latest
