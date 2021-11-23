d957d6b568b2669916fb5acfe20f9f0d46ccdbb7
andrsmllr <andrsmllr@bananatronics.org>
2021-11-23 17:38:52 +0100

    Fix name of docker tag in several build.sh scripts.

--- klayout/build.sh
+++ klayout/build.sh
@@ -2,7 +2,8 @@
 
 DOCKER_BUILD_TARGET="klayout-app"
 DOCKER_IMAGE="andrsmllr/klayout"
+DOCKER_TAG="latest"
 BUILD_VERSION="HEAD"
 
 source ../dockerutils.sh
-docker_build ${DOCKER_BUILD_TARGET} ${DOCKER_IMAGE} ${BUILD_VERSION}
+docker_build ${DOCKER_BUILD_TARGET} ${DOCKER_IMAGE} ${DOCKER_TAG} ${BUILD_VERSION}
--- yosys/build.sh
+++ yosys/build.sh
@@ -2,7 +2,8 @@
 
 DOCKER_BUILD_TARGET="yosys-app"
 DOCKER_IMAGE="andrsmllr/yosys"
+DOCKER_TAG="latest"
 BUILD_VERSION="HEAD"
 
 source ../dockerutils.sh
-docker_build ${DOCKER_BUILD_TARGET} ${DOCKER_IMAGE} ${BUILD_VERSION}
+docker_build ${DOCKER_BUILD_TARGET} ${DOCKER_IMAGE} ${DOCKER_TAG} ${BUILD_VERSION}
